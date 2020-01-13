1fd44f5ea16520b4d0c0ebe666866922da8ab00d
Dizans <dizansyu@gmail.com>
2020-01-13 18:02:21 +0800

    implement common_reduce

---
+++ src/common_reduce.rs
@@ -0,0 +1,56 @@
+use std::io::{self, prelude::*, BufReader};
+use std::fs;
+use std::cmp::Ordering;
+
+use crate::utils::{reduce_name, KeyValue};
+use serde_json;
+
+pub fn do_reduce(job_name: &str, reduce_task: usize, 
+             outfile: &str, n_map: usize,
+             reduce_f: fn(&str, &Vec<String>) -> String){
+    let mut inter_files: Vec<fs::File> = Vec::with_capacity(n_map);
+    for i in 0..n_map{
+        let filename = reduce_name(job_name, i, reduce_task);
+        let file = fs::File::open(filename)
+            .expect("open file failed");
+        inter_files.push(file);
+    }
+
+    let mut kvs: Vec<KeyValue> = Vec::new();
+    for file in inter_files{
+        let reader = BufReader::new(file);
+        for line in reader.lines(){
+            let kv: KeyValue = serde_json::from_str(&line.unwrap())
+                .expect("parse string to kv failed");
+            kvs.push(kv);
+        }        
+    }
+
+    kvs.sort_by(|l, r| l.k.partial_cmp(&r.k).unwrap());
+
+    let mut file = fs::OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open(outfile)
+        .expect("append/create file failed");
+
+    let mut i = 0;
+    while i < kvs.len(){
+        let mut vals: Vec<String> = Vec::new();
+        let mut j = i;
+        while j< kvs.len() && kvs[i].k==kvs[j].k{
+            vals.push(kvs[j].v.clone()); 
+            j+=1;
+        }
+        
+        let result_kv = KeyValue{
+            k: kvs[i].k.clone(),
+            v: reduce_f(&kvs[i].k, &vals)
+        };
+
+        file.write_all(&serde_json::to_string(&result_kv).unwrap().into_bytes())
+            .expect("write failed");
+        file.write_all("\n".as_bytes()).expect("write failed");
+        i = j;
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,40 @@ mod common_map;
 mod master;
 mod utils;
 mod wc;
+mod common_reduce;
 
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 
-use clap::{Arg, App, SubCommand};
+use clap::{App, Arg, SubCommand};
 
 use common_map::do_map;
+use common_reduce::do_reduce;
 
 fn main() {
     let matches = App::new("mapreduce")
         .author("dizansyu dizansyu@gmail.com")
-        .arg(Arg::with_name("files")
-            .required(true)
-            .multiple(true)
-            .help("files waiting to mapreduce"))
+        .arg(
+            Arg::with_name("files")
+                .required(true)
+                .multiple(true)
+                .help("files waiting to mapreduce"),
+        )
         .get_matches();
 
-    let filenames: Vec<_> = matches.values_of("files")
-        .unwrap().collect();
+    let filenames: Vec<_> = matches.values_of("files").unwrap().collect();
 
+    let n_map = 1;
+    let n_reduce = 2;
     for filename in filenames {
         println!("processing: {}", filename);
-        do_map("first_time_map", 1, filename, 2, wc::map);
+        for n in 0..n_map{
+            do_map("word_count", n, filename, n_reduce, wc::map);
+        }
+    }
+
+    for n in 0..n_reduce{
+        do_reduce("word_count", n, "final_result", n_map, wc::reduce);
     }
 }
