0f133ff00cf4c30426f9b883ef0d926f502533c0
Dizans <dizansyu@gmail.com>
2020-02-12 18:55:29 +0800

    implement sequential

--- src/master.rs
+++ src/master.rs
@@ -1,9 +1,13 @@
+#[deny(dead_code)]
 use crate::utils::*;
 use crate::wc;
+use std::thread;
+use crate::common_map::do_map;
+use crate::common_reduce::do_reduce;
 
 pub fn run<F: Fn(JobPhase)>(
-    job_name: &str,
-    files: Vec<String>,
+    job_name: String,
+    files: &Vec<String>,
     n_reduce: usize,
     schedule: F,
     finish: fn(),
@@ -12,35 +16,42 @@ pub fn run<F: Fn(JobPhase)>(
     schedule(JobPhase::MapPhase);
     schedule(JobPhase::ReducePhase);
     finish();
+    // TODO: to merge
     println!("Run finish");
 }
 
 #[allow(non_snake_case)]
 pub fn sequential(
-    job_name: &str,
+    job_name: String,
     files: Vec<String>,
     n_reduce: usize,
     mapF: fn(&str, &str) -> Vec<KeyValue>,
     reduceF: fn(&str, &Vec<String>) -> String,
 ) {
-    run(
-        job_name,
-        files,
-        n_reduce,
-        |phase| match phase {
-            JobPhase::MapPhase => {
-                mapF("1", "2");
-            }
-            JobPhase::ReducePhase => {
-                reduceF("2", &vec!["3".to_owned()]);
-            }
-        },
-        seq_finish,
-    )
+    thread::spawn(move ||
+        run(
+            job_name.clone(),
+            &files,
+            n_reduce,
+            |phase| match phase {
+                JobPhase::MapPhase => {
+                    for (i, f) in files.iter().enumerate(){
+                        do_map(&job_name, i, f, n_reduce, mapF);
+                    }
+                }
+                JobPhase::ReducePhase => {
+                    for i in 0..n_reduce{
+                        do_reduce(&job_name, i, &merge_name(&job_name, i), files.len(), reduceF);
+                    }
+                }
+            },
+            seq_finish,
+        )
+    );
 }
 
 fn seq_finish() {}
 
 fn wc_seq() {
-    sequential("test", vec!["test".to_owned()], 1, wc::map, wc::reduce)
+    sequential("test".to_owned(), vec!["test".to_owned()], 1, wc::map, wc::reduce)
 }
