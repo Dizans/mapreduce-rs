0b67d79e10f5f1ee6045911b58f49cdb7fa0c4fa
Dizans <dizansyu@gmail.com>
2020-02-17 16:37:09 +0800

    code reactor

--- src/common_reduce.rs
+++ src/common_reduce.rs
@@ -1,29 +1,32 @@
-use std::io::{self, prelude::*, BufReader};
-use std::fs;
 use std::cmp::Ordering;
+use std::fs;
+use std::io::{self, prelude::*, BufReader};
 
 use crate::utils::{reduce_name, KeyValue};
 use serde_json;
 
-pub fn do_reduce(job_name: &str, reduce_task: usize, 
-             outfile: &str, n_map: usize,
-             reduce_f: fn(&str, &Vec<String>) -> String){
+pub fn do_reduce(
+    job_name: &str,
+    reduce_task: usize,
+    outfile: &str,
+    n_map: usize,
+    reduce_f: fn(&str, &Vec<String>) -> String,
+) {
     let mut inter_files: Vec<fs::File> = Vec::with_capacity(n_map);
-    for i in 0..n_map{
+    for i in 0..n_map {
         let filename = reduce_name(job_name, i, reduce_task);
-        let file = fs::File::open(filename)
-            .expect("open file failed");
+        let file = fs::File::open(filename).expect("open file failed");
         inter_files.push(file);
     }
 
     let mut kvs: Vec<KeyValue> = Vec::new();
-    for file in inter_files{
+    for file in inter_files {
         let reader = BufReader::new(file);
-        for line in reader.lines(){
-            let kv: KeyValue = serde_json::from_str(&line.unwrap())
-                .expect("parse string to kv failed");
+        for line in reader.lines() {
+            let kv: KeyValue =
+                serde_json::from_str(&line.unwrap()).expect("parse string to kv failed");
             kvs.push(kv);
-        }        
+        }
     }
 
     kvs.sort_by(|l, r| l.k.partial_cmp(&r.k).unwrap());
@@ -35,17 +38,17 @@ pub fn do_reduce(job_name: &str, reduce_task: usize,
         .expect("append/create file failed");
 
     let mut i = 0;
-    while i < kvs.len(){
+    while i < kvs.len() {
         let mut vals: Vec<String> = Vec::new();
         let mut j = i;
-        while j< kvs.len() && kvs[i].k==kvs[j].k{
-            vals.push(kvs[j].v.clone()); 
-            j+=1;
+        while j < kvs.len() && kvs[i].k == kvs[j].k {
+            vals.push(kvs[j].v.clone());
+            j += 1;
         }
-        
-        let result_kv = KeyValue{
+
+        let result_kv = KeyValue {
             k: kvs[i].k.clone(),
-            v: reduce_f(&kvs[i].k, &vals)
+            v: reduce_f(&kvs[i].k, &vals),
         };
 
         file.write_all(&serde_json::to_string(&result_kv).unwrap().into_bytes())
